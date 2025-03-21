d69ff47cdebcbe7761b30209057fe96b5fef7998
Kaif0506 <kaifmondal0506@gmail.com>
2025-03-22 00:16:26 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const express = require('express')
 const mongoose = require('mongoose');
 const router = require('./routes/router')
 const cors = require('cors');
+const path = require("path");
+
 
 
 
@@ -14,12 +16,24 @@ app.use(cors());  // Enable CORS for all routes
 
 app.use(express.json()) 
 
+app.use(express.static(path.join(__dirname, "build"))); // Adjust the folder if needed
+app.use(express.static(path.resolve(__dirname, "build")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log('Connected to MongoDB'))
 .catch((err)=>console.log(err));
 
 app.use('/api', router);
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 app.get('/hello',(req,res)=>{
     res.send('Hello World!')
 })
